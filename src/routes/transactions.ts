8e4c5078e53ec2388c175022388c79ff8cb24e71
Newton Duarte <newton_@hotmail.com>
2024-03-27 11:00:54 -0300

    feat: add get transaction by id route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,23 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
     const transactions = await knex('transactions').select()
 
     return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
   })
 }
